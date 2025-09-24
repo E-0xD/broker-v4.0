fd8a913671333494bf88de5c21b5e37baf5e8035
E-0xD <danielewere99@gmail.com>
2025-09-24 17:11:15 +0100

    Refactor user settings interface: update profile and password forms with improved layout and user feedback

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -30,7 +30,7 @@
                   </a>
               </li>
 
-                 <li class="side-nav-item">
+              <li class="side-nav-item">
                   <a href="{{ route('chart') }}" class="side-nav-link">
                       <span class="menu-icon"><i class="ti ti-dashboard"></i></span>
                       <span class="menu-text"> Live Charts </span>
@@ -38,15 +38,15 @@
               </li>
 
               <li class="side-nav-item">
-                  <a data-bs-toggle="collapse" href="#sidebarHospital" aria-expanded="false"
-                      aria-controls="sidebarHospital" class="side-nav-link">
+                  <a data-bs-toggle="collapse" href="#sidebarInvestment" aria-expanded="false"
+                      aria-controls="sidebarInvestment" class="side-nav-link">
                       <span class="menu-icon"><i class="ti ti-medical-cross"></i></span>
                       <span class="menu-text">
                           investment
                       </span>
                       <span class="menu-arrow"></span>
                   </a>
-                  <div class="collapse" id="sidebarHospital">
+                  <div class="collapse" id="sidebarInvestment">
                       <ul class="sub-menu">
                           <li class="side-nav-item">
                               <a href="{{ route('investment.create') }}" class="side-nav-link">
@@ -82,6 +82,32 @@
                       <span class="menu-icon"><i class="ti ti-building-hospital"></i></span>
                       <span class="menu-text"> Transaction History </span>
                   </a>
+              </li>
+
+              <li class="side-nav-item">
+                  <a data-bs-toggle="collapse" href="#sidebarSettings" aria-expanded="false"
+                      aria-controls="sidebarSettings" class="side-nav-link">
+                      <span class="menu-icon"><i class="ti ti-medical-cross"></i></span>
+                      <span class="menu-text">
+                          Settings
+                      </span>
+                      <span class="menu-arrow"></span>
+                  </a>
+                  <div class="collapse" id="sidebarSettings">
+                      <ul class="sub-menu">
+                          <li class="side-nav-item">
+                              <a href="{{ route('settings.profile') }}" class="side-nav-link">
+                                  <span class="menu-text">Update Profile</span>
+                              </a>
+                          </li>
+                          <li class="side-nav-item">
+                              <a href="{{ route('settings.password') }}" class="side-nav-link">
+                                  <span class="menu-text">Update Password</span>
+                              </a>
+                          </li>
+
+                      </ul>
+                  </div>
               </li>
 
           </ul>
